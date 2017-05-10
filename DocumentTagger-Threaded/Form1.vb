97f27f2a672f70643e6c6671e4a7285f64059e53
eljacobsen12 <ejacobsen@mwmanufacturers.com>
2017-05-10 14:43:22 -0400

    Add project files.

---
+++ DocumentTagger-Threaded/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+        Dim F2 As New Form2 'Create New Form2 Object
+        Dim strText As String = "Thread Is Running!" 'Text To Display
+        Dim lbList As New ListBox 'Create New ListBox, Used By Form 2
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            'Create Thread, and Specify Delegate
+            Dim tThread1 As New Thread(AddressOf ThreadProcedure)
+            'Start A Thread
+            tThread1.Start()
+            'Show Form 2
+            F2.Show()
+            F2.TopMost = True
+        End Sub
+
+        'What Thread 1 Must Do
+        Private Sub ThreadProcedure()
+            'Specify Properties For ListBox
+            lbList.Location = New Point(0, 0)
+            lbList.Width = 300
+            lbList.Height = 300
+            lbList.Items.Add(strText) 'Add Text
+            'Call Delegate To Add ListBox To A Different Thread
+            AddControlToForm(lbList)
+        End Sub
+
+        'Delegate
+        Private Delegate Sub AddListBox(ByVal ctrToAdd As Control)
+
+        Private Sub AddControlToForm(ByVal ctrl As Control)
+            'InvokeRequired Informs Us If We Are In Correct Thread
+            If Me.InvokeRequired Then 'Not
+                'Create Another Delegate
+                Dim TempDelegate As New AddListBox(AddressOf AddControlToForm)
+                'Store Parameters - OPTIONAL
+                Dim parameters(0) As Object
+                parameters(0) = ctrl
+                'Invoke TempDelegate
+                Me.Invoke(TempDelegate, parameters)
+            Else 'Yes
+                F2.Controls.Add(ctrl)
+            End If
+        End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ThreadProcedure() 'Run Separate Thread
+    End Sub
+End Class
---
+++ DocumentTagger-Threaded/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DocumentTagger-Threaded")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DocumentTagger-Threaded")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f86efd1d-3280-4e70-a5c1-5c60f2443523")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
